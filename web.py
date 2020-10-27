b7ef88b619587772f5306f5c6742bfea7c002829
tomioe <tobias.oestergren@ieee.org>
2020-10-27 16:04:59 +0100

    Merged all info into a single page

--- web.py
+++ web.py
@@ -15,53 +15,10 @@ from flask import (
 app = Flask(__name__)
 app.secret_key = b'd02c4c4cde7ae76252540d116a40f23a' # hello github crawler!
 
-'''
-    TODO:
-        * Generic route for wifi / server
-        * Merge into single page
-'''
 
-
-@app.route('/server', methods=('GET', 'POST'))
-def server():
-    print('server route')
-    if len(config.read(cvar.CONFIG_FILENAME)) == 0:
-        print('\tno config read, going back to /')
-        flash('no config read yet')
-        return redirect('/')
-
-
-    if request.method == 'POST':
-        print('\tgot server post')
-        new_server_addr = request.form['server']
-        new_server_port = request.form['port']
-        error = None
-
-        if not new_server_addr or not new_server_port:
-            print('\terror in entered data')
-            error = 'address & port is required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            # write to status.ini file
-            config[cvar.CONFIG_SECTION][cvar.CONFIG_SERV_ADDR] = new_server_addr
-            config[cvar.CONFIG_SECTION][cvar.CONFIG_SERV_PORT] = new_server_port
-            cvar.write_config(config)
-            print('\twrote new server config')
-            flash('new wifi config written, attempting to connect...')
-
-    server_properties = {}
-    server_properties['config_server_address'] = config[cvar.CONFIG_SECTION][cvar.CONFIG_SERV_ADDR]
-    server_properties['config_server_port'] = config[cvar.CONFIG_SECTION][cvar.CONFIG_SERV_PORT]
-
-
-    return render_template('server.html', server_properties=server_properties)
-    
-
-@app.route('/wifi', methods=('GET', 'POST'))
+@app.route('/info', methods=('GET', 'POST'))
 def wifi():
-    print('wifi route')
+    print('page route')
     if len(config.read(cvar.CONFIG_FILENAME)) == 0:
         print('\tno config read, going back to /')
         flash('no config read yet')
@@ -71,35 +28,45 @@ def wifi():
         print('\tgot wifi post')
         new_ssid = request.form['ssid']
         new_pw = request.form['pw']
-        error = None
+        new_addr = request.form['address']
+        new_port = request.form['port']
 
-        if not new_ssid or not new_pw:
-            print('\terror in entered data')
-            error = 'ssid and pw is required.'
-
-        if error is not None:
-            flash(error)
+        if not new_ssid:
+            print('\tno wifi ssid')
+            flash('wifi ssid is required.')
+        elif not new_pw:
+            print('\tno wifi pw')
+            flash('wifi pw is required.')
+        elif not new_addr: 
+            print('\tno server addr')
+            flash('server addr is required.')
+        elif not new_port: 
+            print('\tno server port')
+            flash('server port is required.')
         else:
             # write to status.ini file
             print('\twrote new wifi config')
-            flash('new wifi config written, attempting to connect...')
+            flash('new config written, attempting to connect...')
             config[cvar.CONFIG_SECTION][cvar.CONFIG_CURR_SSID] = new_ssid
             config[cvar.CONFIG_SECTION][cvar.CONFIG_CURR_PW] = new_pw
+            config[cvar.CONFIG_SECTION][cvar.CONFIG_SERV_ADDR] = new_addr
+            config[cvar.CONFIG_SECTION][cvar.CONFIG_SERV_PORT] = new_port
             cvar.write_config(config)
 
-    wifi_properties = {}
-    wifi_properties['config_ssid'] = config[cvar.CONFIG_SECTION][cvar.CONFIG_CURR_SSID]
-    wifi_properties['config_pw'] = config[cvar.CONFIG_SECTION][cvar.CONFIG_CURR_PW]
+    page_properties = {}
+    page_properties['config_ssid'] = config[cvar.CONFIG_SECTION][cvar.CONFIG_CURR_SSID]
+    page_properties['config_pw'] = config[cvar.CONFIG_SECTION][cvar.CONFIG_CURR_PW]
+    page_properties['config_server_address'] = config[cvar.CONFIG_SECTION][cvar.CONFIG_SERV_ADDR]
+    page_properties['config_server_port'] = config[cvar.CONFIG_SECTION][cvar.CONFIG_SERV_PORT]
 
-
-    return render_template('wifi.html', wifi_properties=wifi_properties)
+    return render_template('info.html', page_properties=page_properties)
 
 @app.route('/')
 def default():
     '''
         1) Read the config file
         2) determine what the 'status' is
-        3) server template based on this value
+        3) render info template
     '''
     print('root route')
     x = config.read(cvar.CONFIG_FILENAME)
@@ -107,14 +74,13 @@ def default():
         print("\tconfig file was read")
         curr_status = config[cvar.CONFIG_SECTION][cvar.CONFIG_SERVER_STATUS]
         if curr_status == 'no_conn':
-            print('\twe\'ve got no wifi connection')
-            return redirect('/wifi')
-        elif curr_status == 'no_resp'
-            print('\twe\'ve got no server connection')
-            return redirect('/server')
+            print('\tno wifi connection')
+        elif curr_status == 'no_resp':
+            print('\tno server connection')
         elif curr_status == 'ok':
             print('\tserver connection is ok')
-            return redirect('/server')
+
+        return redirect('/info')
     else:
         flash('no config file created yet')
         print(f'x was {x} , filename was {cvar.CONFIG_FILENAME}')
