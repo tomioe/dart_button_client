21dc916b7c8f3ff098d7683b2458d4425ed813b4
Tobias Østergren <tobias.oestergren@ieee.org>
2021-03-05 21:55:51 +0100

    better install messages

--- install.sh
+++ install.sh
@@ -6,13 +6,18 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
+echo "Copying unit files to systemd"
 cp ./config/*.* /lib/systemd/system/
+echo "Creating empty config logs"
 touch web.log
 touch controller.log
+echo "Installing python req's"
 rm -rf status.ini
 pip3 install -r requirements.txt
 
+echo "Install AP & Wlan scripts"
 sudo chmod 777 ./ApWlanScripts/*.sh
 # run script that allows for easy switching between AP and WLAN
 # since we set the WLAN through web interface, no need to set -s[sid] and -p[assword]
 ./ApWlanScripts/_setup_wlan_and_AP_modes.sh -s "" -p "" -a Dartbutton -r dartbutton
+echo "Run 'sudo reboot now'"
